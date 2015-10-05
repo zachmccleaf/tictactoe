83ad816a1b8dd852403e1c34c06fff932a782868
Zach McCleaf <zmccleaf@andculture.com>
2015-10-05 16:43:59 -0400

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TTT.Startup))]
+namespace TTT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
